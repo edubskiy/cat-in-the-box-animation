31988fd921802e05a204926714c509e3cce3e60c
Evgeniy Dubskiy <edubskiy@openwaygroup.com>
2020-07-14 10:52:39 +0300

    Added first cat animation from top to bottom with Animation/AnimationController/Tween

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cat_in_the_box/src/widgets/cat.dart';
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(CurvedAnimation(
+      parent: catController,
+      curve: Curves.easeIn,
+    ));
+
+    catController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation!'),
+      ),
+      body: buildAnimation(),
+    );
+  }
+
+  Widget buildAnimation() {
+    return AnimatedBuilder(
+        animation: catAnimation,
+        builder: (context, child) {
+          return Container(
+              child: child, margin: EdgeInsets.only(top: catAnimation.value));
+        },
+        child: Cat());
   }
 }
